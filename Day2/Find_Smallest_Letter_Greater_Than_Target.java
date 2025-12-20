6e1c79d44221db66862f463761ef77b6418e26f6
Sohit62664 <sohit662006@gmail.com>
2025-12-20 11:15:21 +0530

    Day 2: Add Find Smallest Letter Greater Than Target solution

---
+++ Day2/Find_Smallest_Letter_Greater_Than_Target.java
@@ -0,0 +1,45 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+
+        //method 01
+
+        // int st= 0;
+        // int end = letters.length-1;
+
+        // while(st<= end){
+        //     int mid= st+ (end-st)/2;
+        //     // if(letters[mid]== target) return letters[st];
+        //     if(letters[mid] > target) end= mid-1;
+        //     else st=mid+1;
+        // }
+        // return letters[st% letters.length]; 
+
+
+
+        //method 02
+        target++;
+        for(int i= 0 ; i< 26; i++){
+            int flag= bin_search(letters, target++);
+            if(flag!=-1){
+                return letters[flag];
+            }
+        }
+        return letters[0];
+
+    }
+    public static int bin_search(char[] letters , char target){
+        int st=0;
+        int end=letters.length-1;
+        while(end>=st){
+            int mid = st+ (end-st)/2;
+            if(letters[mid]==target){return mid;}
+            else if(letters[mid]>target){
+                end= mid-1;
+            }else{
+                st=mid+1;
+            }
+        }
+
+        return -1;
+    }
+}
