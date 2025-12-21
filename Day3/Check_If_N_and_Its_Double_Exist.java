246ea2ae4566f462df7cb978d0bee8ea209c632e
Sohit62664 <sohit662006@gmail.com>
2025-12-21 19:03:28 +0530

    With method no.02

--- Day3/Check_If_N_and_Its_Double_Exist.java
+++ Day3/Check_If_N_and_Its_Double_Exist.java
@@ -1,17 +1,38 @@
 package Day3;
 
+import java.util.HashSet;
+
 public class Check_If_N_and_Its_Double_Exist {
     class Solution {
-        public boolean checkIfExist(int[] arr) {
-            for (int i = 0; i < arr.length; i++) {
-                for (int j = 0; j < arr.length; j++) {
-                    if (i != j && arr[i] == 2 * arr[j]) {
-                        return true;
-                    }
-                }
-            }
-            return false;
+    public boolean checkIfExist(int[] arr) {
+
+        // method 01
+        // for (int i = 0; i < arr.length; i++) {
+        //     for (int j = 0; j < arr.length; j++) {
+        //         if (i != j && arr[i] == 2 * arr[j]) {
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        //method 02
+        HashSet<Integer> set = new HashSet<>();
+        int count=0;
+        for (int a : arr) {
+            set.add(a);
+            if(a==0) count++;
         }
+        if(count>=2) return true;
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] !=0 && set.contains(arr[i] * 2)) {
+                return true;
+            }
+        }
+        return false;
     }
+}
+
 
 }
