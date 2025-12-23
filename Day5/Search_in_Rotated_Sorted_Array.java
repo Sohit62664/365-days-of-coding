8f51f7beb1aea1da1f2ed0e68a0e0f6c19912804
Sohit62664 <sohit662006@gmail.com>
2025-12-23 16:49:43 +0530

    Day5 Search_in_Rotated_Sorted_Array Solution

---
+++ Day5/Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,40 @@
+package Day5;
+
+public class Search_in_Rotated_Sorted_Array {
+    
+class Solution1 {
+    public int search(int[] nums, int target) {
+        int st = 0;
+        int end = nums.length - 1;
+
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            //left sorted
+            if (nums[st] <= nums[mid]) {
+                if (nums[st] <= target && nums[mid] > target) {
+                    end = mid - 1;
+                } else {
+                    st = mid + 1;
+                }
+            } else {
+                //right Sorted
+                if (nums[mid] < target && nums[end] >= target) {
+                    st = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+}
+
+
+
+
+}
