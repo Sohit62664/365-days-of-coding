2590a0346bc49cd1c74cb07763b6b99da7b9c6c0
Sohit Sahu <sohit662006@gmail.com>
2025-12-23 16:52:38 +0530

    Implement search using pivot in rotated array

--- Day5/Search_in_Rotated_Sorted_Array.java
+++ Day5/Search_in_Rotated_Sorted_Array.java
@@ -34,7 +34,85 @@ class Solution1 {
 
 }
 
+//Second method By Finding pivot 
+class Solution2 {
+    public int search(int[] nums, int target) {
+        int pivot = findpivot(nums);
+        // int st = 0;
+        // int end = pivot;
 
+        // pivot not found case
+        if (pivot == -1) {
+            return bin_search(nums, 0, nums.length - 1, target);
+        }
+        // if the pivot is findout then their is three caces 
+        if(nums[pivot]== target){
+            return pivot;
+        }
+        if (target >= nums[0]) {
+            return bin_search(nums, 0, pivot - 1, target);
+        }
 
+        return bin_search(nums, pivot + 1, nums.length - 1, target);
+
+        // while (st <= end) {
+        //     int mid = st + (end - st) / 2;
+        //     if (nums[mid] == target) {
+        //         return mid;
+        //     } else if (nums[mid] > target) {
+        //         end = mid - 1;
+        //     } else {
+        //         st = mid + 1;
+        //     }
+        // }
+
+        // st = findpivot(nums);
+        // end = nums.length - 1;
+        // while (st <= end) {
+        //     int mid = st + (end - st) / 2;
+        //     if (nums[mid] == target) {
+        //         return mid;
+        //     } else if (nums[mid] > target) {
+        //         end = mid - 1;
+        //     } else {
+        //         st = mid + 1;
+        //     }
+        // }
+        // return -1;
+    }
+
+    static int bin_search(int[] nums, int st, int end, int target) {
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                st = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    static int findpivot(int[] nums) {
+        int st = 0;
+        int end = nums.length - 1;
+
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (mid < end && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (mid > st && nums[mid] < nums[mid - 1]) {
+                return mid - 1;
+            } else if (nums[st] >= nums[mid]) {
+                end = mid - 1;
+            } else {
+                st = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
 
 }
