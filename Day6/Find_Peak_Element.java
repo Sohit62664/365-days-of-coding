775bed3b8025645a00dd7c6111277b0ebdfe41ae
Sohit62664 <sohit662006@gmail.com>
2025-12-24 11:44:35 +0530

    Day 6 Find Peak Element Solution

---
+++ Day6/Find_Peak_Element.java
@@ -0,0 +1,20 @@
+package Day6;
+
+public class Find_Peak_Element {
+    class Solution {
+        public int findPeakElement(int[] arr) {
+            int st = 0;
+            int end = arr.length - 1;
+            while (st < end) {
+                int mid = st + (end - st) / 2;
+                if (arr[mid] > arr[mid + 1]) {
+                    end = mid;
+                } else {
+                    st = mid + 1;
+                }
+            }
+            return end;
+        }
+    }
+
+}
