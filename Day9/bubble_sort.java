303fe2c004d4d997aad1e6536fec20c4ee89d2e3
Sohit62664 <sohit662006@gmail.com>
2025-12-27 10:58:55 +0530

    Day 8 Bubble sort Solution

---
+++ Day9/bubble_sort.java
@@ -0,0 +1,32 @@
+package Day9;
+
+public class bubble_sort {
+    class Solution {
+        public void bubbleSort(int[] arr) {
+            // code here
+            // run till the n-1 steps
+            int n = arr.length;
+            boolean swaped;
+
+            for (int i = 0; i < n; i++) {
+                //
+                swaped = false;
+                for (int j = 1; j < n - i; j++) {
+
+                    // check for swaped
+                    if (arr[j] < arr[j - 1]) {
+                        int temp = arr[j];
+                        arr[j] = arr[j - 1];
+                        arr[j - 1] = temp;
+                        swaped = true;
+                    }
+                }
+
+                if (!swaped) {
+                    break;
+                }
+            }
+            return;
+        }
+    }
+}
