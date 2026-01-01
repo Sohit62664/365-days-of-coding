689a3289dfe4583378708294c45d91737ed9c59a
Sohit62664 <sohit662006@gmail.com>
2026-01-01 16:35:04 +0530

    Day 14 First Unique Char Solution

---
+++ Day14/firstUniqChar.java
@@ -0,0 +1,24 @@
+package Day14;
+
+import java.util.HashMap;
+
+public class firstUniqChar {
+    class Solution {
+        public int firstUniqChar(String s) {
+            // find out First non repiting characterarray
+
+            HashMap<Character, Integer> map = new HashMap<>();
+
+            for (char c : s.toCharArray()) {
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            }
+
+            for (int i = 0; i < s.length(); i++) {
+                if (map.get(s.charAt(i)) == 1)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
