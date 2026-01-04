896e685a7026672ca3ae5046d8f03e81d60b95d7
Sohit62664 <sohit662006@gmail.com>
2026-01-04 23:42:37 +0530

    If the Array Does not Contins 0

--- Day17/Missing_number.java
+++ Day17/Missing_number.java
@@ -30,4 +30,36 @@ public class Missing_number {
             arr[second] = temp;
         }
     }
+
+    // if the array Is not contains 0 then we can use this approach
+    class Solution2 {
+        int missingNum(int[] nums) {
+
+            int i = 0;
+            while (i < nums.length) {
+                int correct = nums[i] - 1;
+
+                if (correct < 0 || correct >= nums.length || nums[i] == nums[correct]) {
+                    i++;
+                } else {
+                    swap(nums, i, correct);
+                }
+            }
+
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] != j + 1) {
+                    return j + 1;
+                }
+            }
+
+            return nums.length + 1;
+        }
+
+        void swap(int[] arr, int i, int j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
 }
