697e5a1e41ca1b3ce72536b6683a4c92bb77f6fe
Sohit62664 <sohit662006@gmail.com>
2026-01-05 15:45:34 +0530

    Find All Duplicates By Cyclic Sort

---
+++ Day18/Find_All_Duplicate_by_cyclic_sort.java
@@ -0,0 +1,35 @@
+package Day18;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Find_All_Duplicate_by_cyclic_sort {
+    class Solution {
+        public List<Integer> findDuplicates(int[] nums) {
+            int i = 0;
+            while (i < nums.length) {
+                int correct = nums[i] - 1;
+                if (nums[i] != nums[correct]) {
+                    swap(nums, i, correct);
+                } else {
+                    i++;
+                }
+            }
+            List<Integer> ans = new ArrayList<>();
+
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] != j + 1) {
+                    ans.add(nums[j]);
+                }
+            }
+            return ans;
+        }
+
+        public void swap(int[] arr, int first, int second) {
+            int temp = arr[first];
+            arr[first] = arr[second];
+            arr[second] = temp;
+        }
+
+    }
+}
