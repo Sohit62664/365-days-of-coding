a80fc8ba57b346bf2e6a2a07737a38040c68ad79
Sohit62664 <sohit662006@gmail.com>
2026-01-06 23:55:29 +0530

    Reverce String Solution

---
+++ Day19/Reverce_String.java
@@ -0,0 +1,14 @@
+package Day19;
+
+public class Reverce_String {
+    class Solution {
+        public void reverseString(char[] s) {
+            StringBuilder sb = new StringBuilder(new String(s));
+            sb.reverse();
+
+            for (int i = 0; i < s.length; i++) {
+                s[i] = sb.charAt(i);
+            }
+        }
+    }
+}
