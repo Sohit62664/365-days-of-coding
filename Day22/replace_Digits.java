9cc4c31103171db0d1366028645e59115f0cc2e5
Sohit62664 <sohit662006@gmail.com>
2026-01-11 20:28:08 +0530

    Replace_Digits Solution

---
+++ Day22/replace_Digits.java
@@ -0,0 +1,24 @@
+package extra;
+
+public class replace_Digits {
+    class Solution {
+        public String replaceDigits(String s) {
+            StringBuilder ans = new StringBuilder();
+
+            for (int i = 0; i < s.length(); i++) {
+                if (i % 2 == 0) {
+                    ans.append(s.charAt(i)); // String.valueOf(primitive)
+                } else {
+                    ans.append(shift(s.charAt(i - 1), Integer.parseInt(String.valueOf(s.charAt(i)))));
+                }
+            }
+
+            return ans.toString();
+        }
+
+        char shift(char a, int n) {
+            return (char) (a + n);
+        }
+    }
+
+}
