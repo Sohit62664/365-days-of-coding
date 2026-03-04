2e8a8c610337c9f7edd7542ee8b1e755458395ab
Sohit Sahu <sohit662006@gmail.com>
2026-03-04 23:50:54 +0530

    Time: 2 ms (94.84%) | Memory: 47.1 MB (83.45%) - LeetSync

--- Day72-04-03-26/1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
+++ Day72-04-03-26/1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -1,43 +1,80 @@
+// // O(M∗N∗(M+N))
+
+// class Solution {
+//     public int numSpecial(int[][] mat) {
+//         // HashSet<Integer> row = new HashSet<>();
+//         // HashSet<Integer> column = new HashSet<>();
+//         int count= 0 ;
+//         for(int i =0 ; i< mat.length ; i++){
+//             for(int j = 0 ; j< mat[0].length ; j++){
+//                 // if(row.contains(i)){
+//                 //     break;
+//                 // }else 
+//                 if(mat[i][j]==1){
+//                     // row.add(i);
+//                     // column.add(j);
+//                     if(checkrow(mat , i , j) && checkcolumn(mat ,i ,j) ){
+//                         count++;
+//                     }
+//                 }
+//             }
+//         }
+//         return count;
+
+//     }
+
+//     boolean checkrow(int [][] mat , int i , int j ){
+        
+//         for(int k =0 ;k< mat[0].length ; k++){
+//             if(mat[i][k]==1 && k!=j){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     boolean checkcolumn(int [][] mat , int i , int j ){
+        
+//         for(int k =0 ;k< mat.length ; k++){
+//             if(mat[k][j]==1 && k!=i){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
+// O(m*n)
+
 class Solution {
     public int numSpecial(int[][] mat) {
-        // HashSet<Integer> row = new HashSet<>();
-        // HashSet<Integer> column = new HashSet<>();
-        int count= 0 ;
-        for(int i =0 ; i< mat.length ; i++){
-            for(int j = 0 ; j< mat[0].length ; j++){
-                // if(row.contains(i)){
-                //     break;
-                // }else 
-                if(mat[i][j]==1){
-                    // row.add(i);
-                    // column.add(j);
-                    if(checkrow(mat , i , j) && checkcolumn(mat ,i ,j) ){
-                        count++;
-                    }
+
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[] row = new int[m];
+        int[] col = new int[n];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j] == 1){
+                    row[i]++;
+                    col[j]++;
                 }
             }
         }
+
+        int count = 0;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j] == 1 && row[i] == 1 && col[j] == 1){
+                    count++;
+                }
+            }
+        }
+
         return count;
-
-    }
-
-    boolean checkrow(int [][] mat , int i , int j ){
-        
-        for(int k =0 ;k< mat[0].length ; k++){
-            if(mat[i][k]==1 && k!=j){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    boolean checkcolumn(int [][] mat , int i , int j ){
-        
-        for(int k =0 ;k< mat.length ; k++){
-            if(mat[k][j]==1 && k!=i){
-                return false;
-            }
-        }
-        return true;
     }
 }
