daf6f9b856d18186a66a1a85de03465a53c151ab
Sohit Sahu <sohit662006@gmail.com>
2026-03-25 19:18:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (98.71%) - LeetSync

---
+++ Day-91-25-03-26/1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int fib(int n) {
+        return helper(n, 0, 1);
+    }
+
+    int helper(int n, int a, int b) {
+        if (n == 0) return a;
+        return helper(n - 1, b, a + b);
+    }
+}
