cd2b09317d6a17b056dfc4613e191c123831eddc
Sohit Sahu <sohit662006@gmail.com>
2026-03-26 21:40:54 +0530

    Time: 8 ms (92.02%) | Memory: 46.2 MB (38.88%) - LeetSync

---
+++ Day92-26-03-26/326-power-of-three/power-of-three.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        return check(n);
+
+    }
+
+    boolean check(int n) {
+        if (n == 1)
+            return true;
+        if (n <= 0) {
+            return false;
+        }
+        if (n % 3 == 0) {
+            return check(n / 3);
+        } else {
+            return false;
+        }
+
+    }
+}
