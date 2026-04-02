268ef521804fdb0c3f707483021ab8b0d7fc52ea
Sohit Sahu <sohit662006@gmail.com>
2026-04-02 23:16:40 +0530

    Time: 3 ms (34.57%) | Memory: 45.8 MB (59.77%) - LeetSync

---
+++ Day99-02-04-2026/39-combination-sum/combination-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        DFS(0 , target , result , current , candidates  );
+        return result;
+
+    }
+
+    public void DFS(int st , int target , List<List<Integer>> result , List<Integer> current , int [] candidates){
+        if(target == 0){
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        if(target< candidates[st]){ // this is possible becauce of sorting
+            return;
+        }
+
+        for(int i = st ; i< candidates.length ; i++){
+            current.add(candidates[i]);
+
+            DFS(i , target - candidates[i] , result , current, candidates);
+
+            //backtrack
+            current.remove(current.size() - 1);
+        }
+    }
+}
