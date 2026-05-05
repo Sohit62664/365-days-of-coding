7b5af8d47afb546a6c98b48a73bb7b7de8725d20
Sohit Sahu <sohit662006@gmail.com>
2026-05-05 09:01:53 +0530

    Create invert_tree.java

---
+++ Day132-05-05-2026/invert_tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+//swap the left child and right child of eaxh node;
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        
+        if(root== null) return root ;
+
+        // TreeNode l = root.left;
+        // TreeNode r = root.right;
+
+        TreeNode temp = root.left ;
+        root.left  = root.right ;
+        root.right = temp ;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
