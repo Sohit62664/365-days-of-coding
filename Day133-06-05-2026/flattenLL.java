e9acd5d7c7f16bccdf8120df9ae1935f2af0885b
Sohit Sahu <sohit662006@gmail.com>
2026-05-06 08:55:28 +0530

    adding Method 02
    Added a second method for flattening a binary tree.

--- Day133-06-05-2026/flattenLL.java
+++ Day133-06-05-2026/flattenLL.java
@@ -29,3 +29,29 @@ class Solution {
         prev = root;
     }
 }
+
+
+
+
+//Method 02 
+
+
+class Solution {
+    public void flatten(TreeNode root) {
+        TreeNode current = root;
+        while (current != null) {
+            if (current.left != null) {
+                TreeNode temp = current.left;
+
+                while (temp.right != null) {
+                    temp = temp.right;
+                }
+                temp.right = current.right;
+                current.right = current.left;
+                current.left = null;
+            }
+            current = current.right;
+        }
+
+    }
+}
