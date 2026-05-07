83f320efe435d0b6983644fa5b024949209b56f5
Sohit Sahu <sohit662006@gmail.com>
2026-05-07 08:46:43 +0530

    Fix typo in comment for kthSmallest_in_BST.java

---
+++ Day134-07-05-2026/kthSmallest_in_BST.java
@@ -0,0 +1,33 @@
+
+
+// The Simplest Aproach
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    ArrayList<Integer> list = new ArrayList<>();
+    public int kthSmallest(TreeNode root, int k) {
+        helper(root);
+        Collections.sort(list);
+        return list.get(k-1);
+    }
+
+    void helper(TreeNode root){
+        if(root == null ) return ;
+        list.add(root.val);
+        helper(root.left);
+        helper(root.right);
+    }
+}
