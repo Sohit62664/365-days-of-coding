6c88288732baa74ef55c3e04fda7c685b2ea821a
Sohit Sahu <sohit662006@gmail.com>
2026-05-07 23:18:55 +0530

    Add isSubTree method to check subtree presence
    Implemented isSubTree method to check if one tree is a subtree of another. Added isSame helper method for node comparison.

--- Day134-07-05-2026/isSubTree.java
+++ Day134-07-05-2026/isSubTree.java
@@ -1,3 +1,38 @@
+class Solution {
+
+    public boolean isSubTree(Node root1, Node root2) {
+
+        if(root2 == null) return true;
+        if(root1 == null) return false;
+
+        // check current node
+        if(isSame(root1, root2)) {
+            return true;
+        }
+
+        // search left or right
+        return isSubTree(root1.left, root2) ||
+               isSubTree(root1.right, root2);
+    }
+
+    boolean isSame(Node node1, Node node2) {
+
+        if(node1 == null && node2 == null) return true;
+
+        if(node1 == null || node2 == null) return false;
+
+        if(node1.data != node2.data) return false;
+
+        return isSame(node1.left, node2.left) &&
+               isSame(node1.right, node2.right);
+    }
+}
+
+
+
+
+
+
 
 
 // 186 testcases passed
