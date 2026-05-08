c1606ce90698e3a2486b166165fafb1c65e8adbd
Sohit Sahu <sohit662006@gmail.com>
2026-05-09 00:29:36 +0530

    Create gfgpotd.java

---
+++ Day135-08-5-2026/gfgpotd.java
@@ -0,0 +1,70 @@
+class Solution {
+    public List<String> validParenthesis(String s) {
+        // code here
+        List<String> ans = new ArrayList<>();
+
+        Queue<String> q = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+
+        q.offer(s);
+        visited.add(s);
+
+        boolean found = false;
+
+        while (!q.isEmpty()) {
+
+            String curr = q.poll();
+
+            // Check valid
+            if (isValid(curr)) {
+                ans.add(curr);
+                found = true;
+            }
+
+            // If valid found at current level
+            // don't generate next level
+            if (found) continue;
+
+            for (int i = 0; i < curr.length(); i++) {
+
+                char ch = curr.charAt(i);
+
+                // only remove parentheses
+                if (ch != '(' && ch != ')') continue;
+
+                String next =
+                        curr.substring(0, i) +
+                        curr.substring(i + 1);
+
+                if (!visited.contains(next)) {
+                    visited.add(next);
+                    q.offer(next);
+                }
+            }
+        }
+
+        Collections.sort(ans);
+        return ans;
+    }
+    
+    boolean isValid(String s) {
+
+        int count = 0;
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(') {
+                count++;
+            }
+            else if (ch == ')') {
+
+                count--;
+
+                // more closing brackets
+                if (count < 0) return false;
+            }
+        }
+
+        return count == 0;
+    }
+}
