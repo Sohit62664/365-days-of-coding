4b29d8fefc5cd278dfd4e8a466968a523e1208ed
Sohit Sahu <sohit662006@gmail.com>
2026-05-09 08:29:35 +0530

    Create sumNumbers.java

---
+++ Day136-09-05-2026/sumNumbers.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    ArrayList<Integer> list = new ArrayList<>();
+    
+    public int sumNumbers(TreeNode root) {
+        helper(root , 0);
+
+        int sum = 0;
+        for(int i : list){
+            sum+=i;
+        }
+        return sum;
+    }
+
+    void helper(TreeNode root , int num){
+        if(root == null ) return ;
+
+        num = (num*10 )+ root.val;
+        if(root.left== null && root.right == null ){
+            list.add(num);
+            return;
+        }
+
+        helper(root.left, num);
+        helper(root.right, num);
+    }
+}
