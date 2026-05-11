55cf4d4b6ee5ba349515fe4b0d9be826820ced07
Sohit Sahu <sohit662006@gmail.com>
2026-05-11 15:52:22 +0530

    Update pushZerosToEnd.java

--- Day138-11-05-2026/pushZerosToEnd.java
+++ Day138-11-05-2026/pushZerosToEnd.java
@@ -1,6 +1,28 @@
 class Solution {
     void pushZerosToEnd(int[] arr) {
         // code here
+        int count=0 ;
+        for(int i = 0 ; i< arr.length ; i++){
+            if(arr[i]!= 0){
+                arr[count++]= arr[i];
+                
+            }
+        }
+        
+        while(count < arr.length){
+            arr[count++]= 0 ;
+            
+        }
+    }
+}
+
+
+
+// m02 
+
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        // code here
         // int i =0 ;
         // int j = arr.length-1 ;
         // while (i<j){
