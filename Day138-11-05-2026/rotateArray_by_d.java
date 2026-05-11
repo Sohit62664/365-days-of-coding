a764b7f0824daba22e2370c677defbf2933984a8
Sohit Sahu <sohit662006@gmail.com>
2026-05-11 09:41:57 +0530

    Add rotateArray_by_d.java for array rotation
    Implement array rotation using reversal algorithm.

---
+++ Day138-11-05-2026/rotateArray_by_d.java
@@ -0,0 +1,46 @@
+For left rotation by d, reversal algorithm is: 
+1. Reverse first d elements 
+2. Reverse remaining n-d elements 
+3. Reverse whole array
+reverse(0, d - 1);
+reverse(d, n - 1);
+reverse(0, n - 1);
+
+
+For right rotation by d, reversal algorithm is:
+
+1.Reverse whole array
+2.Reverse first d elements
+3.Reverse remaining n-d elements
+
+reverse(0, n - 1);
+reverse(0, d - 1);
+reverse(d, n - 1);
+
+
+// left (counter-clockwise direction)
+
+
+class Solution {
+	static void rotateArr(int arr[], int d) {
+		// code here
+		int n = arr.length ;
+		d = d%n; // if d< n then remain d if d> n then d = d%n 
+		reverse(0, d - 1, arr);
+		reverse(d, n - 1, arr);
+		reverse(0, n - 1, arr);
+		 
+		return ;
+		
+	}
+	static void reverse(int i, int j, int[] arr) {
+		while (i<j) {
+			int temp = arr[j];
+			arr[j] = arr[i];
+			arr[i] = temp;
+			i++;
+			j--;
+		}
+		return;
+	}
+}
