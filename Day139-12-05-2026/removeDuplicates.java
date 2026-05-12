5aa353f92ac4523b2ec4f24ef9ac7c98368c49f3
Sohit Sahu <sohit662006@gmail.com>
2026-05-12 11:04:16 +0530

    Update removeDuplicates.java

--- Day139-12-05-2026/removeDuplicates.java
+++ Day139-12-05-2026/removeDuplicates.java
@@ -1,3 +1,5 @@
+// using Hashset
+
 class Solution {
     ArrayList<Integer> removeDuplicates(int[] arr) {
         // code here
@@ -14,3 +16,25 @@ class Solution {
         return ans; 
     }
 }
+
+
+//using HashMap 
+
+class Solution {
+	ArrayList<Integer> removeDuplicates(int[] arr) {
+		// code here
+		HashMap<Integer, Integer> map = new HashMap<>();
+		ArrayList<Integer> ans = new ArrayList<>();
+		
+		for (int num : arr) {
+			if (!map.containsKey(num)) {
+				map.put(num, 1);
+				ans.add(num);
+			}
+		}
+		
+		return ans;
+	}
+}
+
+
