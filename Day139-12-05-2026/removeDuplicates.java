3235f157293cea57641663a329ac34acd7ca807a
Sohit Sahu <sohit662006@gmail.com>
2026-05-12 11:09:32 +0530

    Update removeDuplicates.java

--- Day139-12-05-2026/removeDuplicates.java
+++ Day139-12-05-2026/removeDuplicates.java
@@ -38,3 +38,19 @@ class Solution {
 }
 
 
+//O(1) space 
+
+class Solution {
+	ArrayList<Integer> removeDuplicates(int[] arr) {
+		// code here
+		
+		ArrayList<Integer> ans = new ArrayList<>();
+		ans.add(arr[0]);
+		for(int i=1 ; i< arr.length ; i++){
+		    if(arr[i]!= arr[i-1]){
+		        ans.add(arr[i]);
+		    }
+		}
+		return ans;
+	}
+}
