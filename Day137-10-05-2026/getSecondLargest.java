043561affc03a59e5dc8c9df4a7b005dc1c6909c
Sohit Sahu <sohit662006@gmail.com>
2026-05-12 07:50:16 +0530

    Update getSecondLargest.java

--- Day137-10-05-2026/getSecondLargest.java
+++ Day137-10-05-2026/getSecondLargest.java
@@ -26,6 +26,29 @@ class Solution {
     }
 }
 
+// optimized above code single pass
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // code here
+        int first =-1 ;
+        int second =-1 ;
+        
+        for(int num : arr){
+            if(num >first){
+                second = first;
+                first = num;
+                
+            }else if(num >second && num != first){
+                second = num ;
+            }
+        }
+        return second;
+    }
+}
+
+
+
+
 // in two pass approach 
 
 class Solution {
