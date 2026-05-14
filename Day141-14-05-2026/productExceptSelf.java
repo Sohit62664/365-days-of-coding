4860cd3bbfdb9d334fc08d0912e09931dad48bf9
Sohit Sahu <sohit662006@gmail.com>
2026-05-14 17:46:26 +0530

    Update productExceptSelf.java

--- Day141-14-05-2026/productExceptSelf.java
+++ Day141-14-05-2026/productExceptSelf.java
@@ -36,3 +36,37 @@ class Solution {
 		
 	}
 }
+
+
+class Solution {
+	public static int[] productExceptSelf(int arr[]) {
+		// code here
+		// by Array Preprocessing or Suffix-prefix Approach
+		
+// 		check the suffix , prefix naming 
+		int n = arr.length;
+		int [] suff = new int[n];
+		int [] pre = new int[n];
+		
+		int s = 1 ;
+		int p = 1 ;
+		suff[0] = 1 ;
+		pre[n - 1] = 1 ;
+		for (int i = 1; i <n ; i++) {
+			int a = arr[i-1];
+			int b = arr[n - i];
+			s *= a;
+			suff[i] = s ;
+			p *= b;
+			pre[n - i-1] = p;
+			
+		}
+		
+		for (int i = 0 ; i<n ; i++) {
+			arr[i] = pre[i]* suff[i];
+		}
+		
+		return arr;
+		
+	}
+}
