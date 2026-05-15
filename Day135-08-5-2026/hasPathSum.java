0094ccd9e5a634913ca1995082b4cc5c908fff92
Sohit Sahu <sohit662006@gmail.com>
2026-05-15 15:38:42 +0530

    Update hasPathSum.java

--- Day135-08-5-2026/hasPathSum.java
+++ Day135-08-5-2026/hasPathSum.java
@@ -44,3 +44,45 @@ class Solution {
 
 // Note: Read the constraints check the ever possible range of the input 
 // in this case negative value 
+
+
+
+
+// wthout using Flag 
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root== null) return false;
+        return helper(root , targetSum , 0 );
+    }
+
+    boolean helper(TreeNode node , int target , int sum ){
+        if(node== null) return false;
+
+        sum += node.val ;
+
+        if(sum == target && node.left == null && node.right == null){
+            return true;
+        }
+
+        boolean left = helper(node.left , target , sum);
+        boolean right  = helper(node.right , target , sum);
+
+        return left|| right;
+    }
+}
