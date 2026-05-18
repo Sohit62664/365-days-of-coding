751fd27f9c27caaaa332f7ce1ad32f19a90f9996
Sohit Sahu <sohit662006@gmail.com>
2026-05-19 05:10:15 +0530

    Update min_Swap_to_all_ones.java

--- Day146-19-05-2026/min_Swap_to_all_ones.java
+++ Day146-19-05-2026/min_Swap_to_all_ones.java
@@ -31,3 +31,62 @@ class Solution {
 		
 	}
 }
+
+
+
+// Now the question is how we reach to the optimal answer
+// 	Well since we have already deframe our question to find the minimum subarray which having the minimum number of zeros
+// 	So in the pre approach our brute force approach we are solving this problem in big O of N square
+// 	Now we have to think like that the minimum subarray problem how can I optimize it into big O of N
+// 	So there may be many approaches like two pointer approach sliding window approach
+// 	So here we have solved with this sliding window approach
+
+class Solution {
+	public int minSwaps(int[] arr) {
+		// code here
+		int k = 0 ;
+		
+		for (int num : arr) {
+			if (num == 1) {
+				k++;
+			}
+		}
+		
+		if (k == 0)
+			return - 1;
+		int n = arr.length;
+		int ans = Integer.MAX_VALUE ;
+		
+		int i = 0 ;
+		int j = 0 ;
+		
+		int zeros = 0;
+		
+		while (j < n) {
+			
+			if (arr[j] == 0) {
+				zeros++;
+			}
+			
+// 			j++;
+			if (j - i +1  == k) { // WEindow Size Formula
+				ans = Math.min(ans, zeros);
+				j++;
+				if (arr[i] == 0) {
+					zeros--;
+				}
+				i++;
+			}else{
+			    j++;
+			}
+			
+		}
+		
+		return ans;
+		
+	}
+}
+
+// Other approach for this problem may be prefix sum
+// 	And we can transform our question to like find out the maximize the number of ones in a window
+// 	What we can think index based and analyze the index Store all the index of and analyze the gap between them
