b0e4032acdaf74f6758bc63b1c774296db981177
Sohit Sahu <sohit662006@gmail.com>
2026-05-19 04:51:06 +0530

    Update min_Swap_to_all_ones.java

--- Day146-19-05-2026/min_Swap_to_all_ones.java
+++ Day146-19-05-2026/min_Swap_to_all_ones.java
@@ -1,5 +1,7 @@
-Broute force 
-
+// Broute force 
+// First of all count all the number of ones in the array 
+// If the count of ones is equal to zero then return minus one which means that we can't make grouping
+// Now your question has been changed from grouping to all the ones you can reframe like this Find the minimum subarray which having at least size of K which means the size of once and we have to count minimum number of zero in it because that will be your answer The minimum number of zero means this much we can SW so that's the brute force 
 class Solution {
 	public int minSwaps(int[] arr) {
 		// code here
