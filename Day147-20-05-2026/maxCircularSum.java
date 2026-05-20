37d57b15d2099f064a87eff78a11f64926ad8550
Sohit Sahu <sohit662006@gmail.com>
2026-05-20 16:50:53 +0530

    Update maxCircularSum.java

--- Day147-20-05-2026/maxCircularSum.java
+++ Day147-20-05-2026/maxCircularSum.java
@@ -24,3 +24,51 @@ class Solution {
 		
 	}
 }
+
+
+
+
+// optimize approach 
+class Solution {
+	
+	public int maxCircularSum(int arr[]) {
+		
+		int totalSum = 0;
+		
+		// For maximum subarray
+		int currMax = arr[0];
+		int maxSum = arr[0];
+		
+		// For minimum subarray
+		int currMin = arr[0];
+		int minSum = arr[0];
+		
+		for (int i = 0 ; i < arr.length ; i++) {
+			
+			totalSum += arr[i];
+			
+			if (i > 0) {
+				
+				// Kadane for max
+				currMax = Math.max(arr[i],currMax + arr[i]);
+				
+				maxSum = Math.max(maxSum,currMax);
+				
+				// Kadane for min
+				currMin = Math.min(arr[i], currMin + arr[i]);
+				
+				minSum = Math.min(minSum,currMin);
+			}
+		}
+		
+		// all elements negative
+		if (maxSum < 0) {
+			return maxSum;
+		}
+		
+		
+		int circularSum = totalSum - minSum;
+		
+		return Math.max(maxSum, circularSum);
+	}
+}
