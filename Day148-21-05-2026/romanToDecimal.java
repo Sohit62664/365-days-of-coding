bc828e7203d87cb59cd8228e57c7965ff1e1f64c
Sohit Sahu <sohit662006@gmail.com>
2026-05-21 20:02:47 +0530

    Update romanToDecimal.java

--- Day148-21-05-2026/romanToDecimal.java
+++ Day148-21-05-2026/romanToDecimal.java
@@ -27,3 +27,44 @@ class Solution {
         return num;
     }
 }
+
+
+// by using function a little slower 
+class Solution {
+	public int romanToDecimal(String s) {
+		// code here
+		
+		int num = 0;
+		int n = s.length();
+		
+		for (int i = 0 ; i<s.length() ; i++) {
+			char ch = s.charAt(i);
+			if (i + 1<n && value(ch) <value(s.charAt(i + 1))) {
+				num -= value(ch);
+			} else {
+				num += value(ch);
+			}
+			
+		}
+		
+		return num;
+	}
+	int value(char r) {
+		if (r == 'I')
+			return 1;
+		if (r == 'V')
+			return 5;
+		if (r == 'X')
+			return 10;
+		if (r == 'L')
+			return 50;
+		if (r == 'C')
+			return 100;
+		if (r == 'D')
+			return 500;
+		if (r == 'M')
+			return 1000;
+		
+		return - 1;
+	}
+}
