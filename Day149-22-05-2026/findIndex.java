99eb73cd91fb0e892b30668211e78eae591e87bb
Sohit Sahu <sohit662006@gmail.com>
2026-05-22 15:35:38 +0530

    Update findIndex.java

--- Day149-22-05-2026/findIndex.java
+++ Day149-22-05-2026/findIndex.java
@@ -29,3 +29,52 @@ class Solution {
 	}
 	
 }
+
+
+
+
+
+// Optimized approach  n time , n space
+	class Solution {
+	public int findIndex(String s) {
+		// code here
+		// no. of closing in n-k == no. of opening in 0 to k
+		
+		// naive approach
+		int n = s.length();
+		int o_count = 0;
+		int c_count = 0 ;
+		
+		int [] open= new int [n];
+		int [] close= new int [n];
+		
+		for(int i =0 ; i < n ; i++){
+		    int j = n-i-1;
+		    open[i]= o_count; // because of exclusion of the current , writting before updating
+		    if(s.charAt(i)=='('){
+		        o_count++;
+		    }
+		    
+		    if(s.charAt(j)==')'){
+		        c_count++;
+		    }
+		    
+		    close[j]=c_count;
+		}
+		
+		
+		for(int i =0 ; i< n ; i++){
+		    if(open[i]== close[i]){
+		        return i;
+		    }
+		}
+		
+		return n;
+		
+	}
+	
+}
+
+
+
+// more optimize n time with 1 space
