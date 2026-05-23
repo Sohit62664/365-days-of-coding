0d74308f9753e3271d2db4e66ac47f7e55cd7bd3
Sohit Sahu <sohit662006@gmail.com>
2026-05-23 17:59:57 +0530

    Update equal012.java

--- Day150-23-05-2026/equal012.java
+++ Day150-23-05-2026/equal012.java
@@ -26,3 +26,47 @@ class Solution {
         return ct;
     }
 }
+
+
+
+// optimize approach 
+
+
+class Solution {
+    public int equal012(String s) {
+        // code here
+        
+        int n= s.length();
+        
+        HashMap<String , Integer> map = new HashMap<>();
+        int z =0 , o= 0 , t= 0 ;
+        
+        
+        int ans = 0;
+        map.put("0#0",1);
+        
+        for(int i =0 ; i< n ; i++){
+            
+            char ch = s.charAt(i);
+            
+            if(ch == '0' ) z++;
+            else if(ch=='1') o++;
+            else t++;
+            
+            
+            int d1 = z-o;
+            int d2 = z-t;
+            
+            String key = d1+ "#" +d2;
+            
+            if(map.containsKey(key)){
+                ans+=map.get(key);
+            }
+            
+            map.put(key , map.getOrDefault(key , 0)+1);
+        }
+        
+        return ans;
+        
+    }
+}
