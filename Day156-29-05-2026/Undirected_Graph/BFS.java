59f1cdb3eb433f07d4b855705302383b4d38b333
Sohit Sahu <sohit662006@gmail.com>
2026-05-29 04:19:05 +0530

    Update BFS.java

--- Day156-29-05-2026/Undirected_Graph/BFS.java
+++ Day156-29-05-2026/Undirected_Graph/BFS.java
@@ -43,3 +43,60 @@ public class BFS extends Graph {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// Without Extending Class 
+
+
+
+package Undirected_Graph;
+
+import java.util.*;
+
+class BFS {
+
+    static void traverse(Graph g, int source) {
+
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[g.V];
+
+        q.offer(source);
+        visited[source] = true;
+
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            System.out.print(u + " ");
+
+            for (int neigh : g.l[u]) {
+                if (!visited[neigh]) {
+                    visited[neigh] = true;
+                    q.offer(neigh);
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Graph g = new Graph(5);
+
+        g.addEdge(0, 1);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(2, 3);
+        g.addEdge(2, 4);
+
+        BFS.traverse(g, 1);
+    }
+}
