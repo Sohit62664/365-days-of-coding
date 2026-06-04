8266a2297eedddda3e680d9d6c4a61e448e53970
Sohit Sahu <sohit662006@gmail.com>
2026-06-04 21:32:29 +0530

    Create Day162-04-06-2026/gfg.java

---
+++ Day162-04-06-2026/gfg.java
@@ -0,0 +1,15 @@
+class Solution {
+    int maxSubstring(String s) {
+        int maxSoFar = Integer.MIN_VALUE;
+        int currSum = 0;
+
+        for (char ch : s.toCharArray()) {
+            int val = (ch == '0') ? 1 : -1;
+
+            currSum = Math.max(val, currSum + val);
+            maxSoFar = Math.max(maxSoFar, currSum);
+        }
+
+        return (maxSoFar <= 0) ? -1 : maxSoFar;
+    }
+}
