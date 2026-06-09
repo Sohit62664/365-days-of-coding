cffad89e91f63d566595eb7b5466ae9fdb2175f3
Sohit Sahu <sohit662006@gmail.com>
2026-06-09 22:09:24 +0530

    Refactor canSeatAllPeople method for clarity
    Refactored canSeatAllPeople method for improved readability and efficiency. Added checks for adjacent occupied seats and optimized seating logic.

--- Day167-09-06-2026/canSeatAllPeople.java
+++ Day167-09-06-2026/canSeatAllPeople.java
@@ -3,6 +3,12 @@ class Solution {
 
         int count = 0;
         int n = seats.length;
+        
+        for(int i =1 ; i < n ; i++){
+            if(seats[i-1] == 1 && seats[i]== 1){
+                return false;
+            }
+        }
 
         for (int i = 0; i < n; i++) {
 
@@ -31,4 +37,46 @@ class Solution {
 
         return count >= k;
     }
+}
+
+
+
+
+
+
+// clean approach   
+
+
+class Solution {
+	public boolean canSeatAllPeople(int k, int[] seats) {
+		// code here
+		// by array preprocessing
+		
+		int n = seats.length;
+		
+		// if(k == 0) return false;
+		
+		for (int i = 0; i < seats.length - 1; i++) {
+			if (seats[i] == 1 && seats[i + 1] == 1) {
+				return false;
+			}
+		}
+		
+		int count = 0 ;
+		
+		for (int i = 0 ; i < n ; i++) {
+			if (seats[i] == 0) {
+				boolean left = (i == 0 || seats[i - 1] == 0);
+				boolean right = (i == n - 1 || seats[i + 1] == 0);
+				
+				if (left && right) {
+					count++;
+					seats[i] = 1;
+				}
+			}
+		}
+		
+		return count >= k ;
+		
+	}
 }
