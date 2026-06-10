e0e4791c9a276bdd77935732ea8f6c4235b22dc3
Sohit Sahu <sohit662006@gmail.com>
2026-06-10 08:10:52 +0530

    Create Domino_piling.java

---
+++ Day168-10-06-2026/Domino_piling.java
@@ -0,0 +1,13 @@
+import java.util.* ; 
+public class Domino_piling {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in) ;
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+
+      
+        int product = M*N ; 
+        System.out.println(product/2);
+
+    }
+}
