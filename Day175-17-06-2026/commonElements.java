7f05b2c4b4a74199c61841e3abff2286bc7621a6
Sohit Sahu <sohit662006@gmail.com>
2026-06-17 09:34:34 +0530

    Update commonElements.java

--- Day175-17-06-2026/commonElements.java
+++ Day175-17-06-2026/commonElements.java
@@ -1,6 +1,7 @@
 class Solution {
 	public ArrayList<Integer> commonElements(int[] a, int[] b, int[] c) {
 		// code here
+		//First approach
 		ArrayList<Integer> list = helper(a, b);
 		int [] d = new int[list.size()];
 		
@@ -40,5 +41,47 @@ class Solution {
 		}
 		
 		return ans;
+	}
+}
+
+
+// most optimal 
+class Solution {
+	public ArrayList<Integer> commonElements(int[] a, int[] b, int[] c) {
+		// code here
+		// most optimal approach
+		ArrayList<Integer> ans = new ArrayList<>();
+		int l1 = a.length;
+		int l2 = b.length;
+		int l3 = c.length;
+		
+		int i = 0 ;
+		int j = 0 ;
+		int k = 0 ;
+		while (i < l1 && j<l2 && k < l3) {
+			if (a[i] == b[j] && b[j] == c[k]) {
+				int last = ans.size();
+				if (last == 0) {
+					ans.add(a[i]);
+				} else if (ans.get(last - 1) != a[i]) {
+					ans.add(a[i]);
+				}
+				i++;
+				j++;
+				k++;
+			} else {
+				
+				int min = Math.min(a[i], Math.min(b[j], c[k]));
+				
+				if (a[i] == min)
+					i++;
+				if (b[j] == min)
+					j++;
+				if (c[k] == min)
+					k++;
+			}
+		}
+		return ans;
+		
 	}
 }
