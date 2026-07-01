749abece5d06e9b9676f49bc43ddc681a6fdbd82
Sohit Sahu <sohit662006@gmail.com>
2026-07-01 09:44:46 +0530

    O(1) Solution
    return n > 0 && (n & (n-1))== 0 ;

--- Day189-01-07-2026/isPowerOfTwo.java
+++ Day189-01-07-2026/isPowerOfTwo.java
@@ -17,3 +17,31 @@ class Solution {
         
     }
 }
+
+
+
+
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+
+        // return check(n);  // Little bit Slower O(log n)
+
+        // Using bit manupuLATION Faster Approach O(n)
+        return n > 0 && (n & (n-1))== 0 ;
+
+    }
+
+
+
+    // boolean check(int n){
+    //     if(n == 1 ) return true;
+    //     if(n <= 0 ) return false;
+    //     if(n%2 == 0){
+    //         return check(n/2); 
+    //     }else{
+    //         return false;
+    //     }
+        
+    // }
+}
