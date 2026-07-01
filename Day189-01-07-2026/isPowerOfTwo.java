dc71e4352c2de963153cda9baedc1bc31ef2f2d2
Sohit Sahu <sohit662006@gmail.com>
2026-07-01 09:41:31 +0530

    Naive Approach
    using % operator

---
+++ Day189-01-07-2026/isPowerOfTwo.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+
+        return check(n);  // Little bit Slower O(log n)
+    }
+
+
+
+    boolean check(int n){
+        if(n == 1 ) return true;
+        if(n <= 0 ) return false;
+        if(n%2 == 0){
+            return check(n/2); 
+        }else{
+            return false;
+        }
+        
+    }
+}
