c4ba71f58e696a70f5911a67a3599e903a5e9370
Sohit Sahu <sohit662006@gmail.com>
2026-07-23 22:33:25 +0530

    Add brute force solution for unique XOR triplets
    Implemented a brute force approach to calculate unique XOR triplets. Added handling for different array sizes and included a method to find the most significant bit index.

--- DAy205-23-07-2026/uniqueXorTriplets.java
+++ DAy205-23-07-2026/uniqueXorTriplets.java
@@ -17,3 +17,61 @@ class Solution {
         return set.size();
     }
 }
+
+
+
+// optimized approach 
+
+
+
+
+// class Solution {
+//     public int uniqueXorTriplets(int[] nums) {
+
+//         // genrate all triplits wil always cost O(n^3) so we can't genrate all the triplites 
+//         // the clue we have given is the number is permutation from 1 to n 
+//         //minimum n possible way n = array Size 
+//         //how to get others 
+//         //testing each triplits will always cost O(n^3) then what to do ?
+//         //we need somehing where we no need to check each and everyone 
+
+//         // Hint 1 What is the maximum and minimum value we can obtain using the given numbers? 
+
+//         //how this hint is releted ? 
+
+//     }
+// }
+
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        //brute force approach 
+        // genrate all triplits O(n^3)
+
+        int n = nums.length;
+        if (n == 2) {
+            return 2;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            int msb_index = msb(n) + 1;
+            double ans = Math.pow(2, msb_index);
+            
+
+
+            return (int) ans;
+        }
+
+    }
+
+    int msb(int n) {
+        for (int i = 31; i >= 0; i--) {
+            int mask = 1 << i;
+
+            if ((n & mask) != 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
