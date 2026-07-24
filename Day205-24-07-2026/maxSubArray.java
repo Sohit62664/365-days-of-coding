6a48fb5c7f4d079c1c3c999a87c97749bbd40880
Sohit Sahu <sohit662006@gmail.com>
2026-07-24 10:53:06 +0530

    Update maxSubArray.java

--- Day205-24-07-2026/maxSubArray.java
+++ Day205-24-07-2026/maxSubArray.java
@@ -37,3 +37,23 @@ class Solution {
         return max_sum ;
     }
 }
+
+
+
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // Brute force 
+
+        int max_sum = Integer.MIN_VALUE ;
+        int curr_sum = 0 ; 
+        int n = nums.length;
+        for(int i =0 ; i< n ; i ++){
+            curr_sum = Math.max(nums[i] ,curr_sum + nums[i] ); //changed
+            max_sum = Math.max(curr_sum , max_sum);
+        }
+
+        return max_sum ;
+    }
+}
