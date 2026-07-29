16a4aec8d10550ff655946692a038672e2ddcec9
Sohit Sahu <sohit662006@gmail.com>
2026-07-29 17:26:57 +0530

    Adding Optimized approach

--- Day211-229-07-2026/minSubsets.java
+++ Day211-229-07-2026/minSubsets.java
@@ -10,3 +10,34 @@ class Solution {
 	return count;
     }
 }
+
+
+
+
+//optimize Approach
+
+
+class Solution {
+    int minSubsets(int arr[]) {
+        // code here
+        // O(n) Approach
+        HashSet<Integer>  set = new HashSet<>();
+        int count = 0 ;
+        
+        
+        
+        
+        for(int i =0 ; i< arr.length ; i++){ //index variable i of the loop
+            set.add(arr[i]);
+        }
+        
+        for(int num : set ){
+            if(!set.contains(num-1)) count++;
+        }
+        
+        return count;
+        
+        
+        
+    }
+}
