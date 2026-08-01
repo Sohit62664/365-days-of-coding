24226bd4618bf34e338c1908767ed6fc80264f8e
Sohit Sahu <sohit662006@gmail.com>
2026-08-01 21:24:24 +0530

    Create moveZeroes.java

---
+++ Day214-01-08-2026/moveZeroes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n= nums.length;
+
+        
+        int j =0 ;
+
+        for(int i =0 ; i< n ; i++){
+            if(nums[j]!= 0){
+                j++;
+            }else if(nums[i]!=0 && nums[j]==0){
+                swap (i , j , nums);
+                j++;
+            }
+        }
+
+
+    }
+    public void swap(int i , int j , int [] nums){
+        int temp = nums[i]; 
+        nums[i] = nums[j];
+        nums[j]= temp;
+    }
+}
