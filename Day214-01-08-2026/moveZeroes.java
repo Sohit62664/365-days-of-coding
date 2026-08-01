c578fabb331fef307cf3cc4c443f480b59ae392c
Sohit Sahu <sohit662006@gmail.com>
2026-08-01 21:25:09 +0530

    Implement moveZeroes method using while loop

--- Day214-01-08-2026/moveZeroes.java
+++ Day214-01-08-2026/moveZeroes.java
@@ -22,3 +22,34 @@ class Solution {
         nums[j]= temp;
     }
 }
+
+
+
+
+
+
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n= nums.length;
+        int i =0 ; 
+        int j = 0 ; 
+
+        while(i < n ){
+            if(nums[j]!= 0){
+                j++;
+            }else if(nums[i]!= 0 && nums[j]==0) {
+                swap(i, j , nums);
+                j++;
+            }
+            i++;
+        }
+        
+
+    }
+    public void swap(int i , int j , int [] nums){
+        int temp = nums[i]; 
+        nums[i] = nums[j];
+        nums[j]= temp;
+    }
+}
