b8367409644e85438ad62a32698be5048ac7543a
Sohit Sahu <sohit662006@gmail.com>
2026-08-04 23:03:57 +0530

    Create findMissingElements.java

---
+++ Day216-04-08-2026/findMissingElements.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        int min = nums[0];
+        int max = nums[0];
+
+        List<Integer> ans = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : nums){
+            min= Math.min(min , num);
+            max= Math.max(max , num);
+            set.add(num);
+        }
+
+        for(int i = min ; i<= max ; i++){
+            if(!set.contains(i)){
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
