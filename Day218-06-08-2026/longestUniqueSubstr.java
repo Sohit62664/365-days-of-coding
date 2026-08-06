e98bad95cc333f693143e2f37411e520152bbee8
Sohit Sahu <sohit662006@gmail.com>
2026-08-07 00:07:21 +0530

    Update longestUniqueSubstr.java

--- Day218-06-08-2026/longestUniqueSubstr.java
+++ Day218-06-08-2026/longestUniqueSubstr.java
@@ -56,3 +56,48 @@ class Solution {
 	}
 }
 
+
+
+//Brute Force without set 
+
+class Solution {
+	public int longestUniqueSubstr(String s) {
+		// code here
+		// Brute force
+		int max = 0 ;
+		for (int i = 0 ; i<s.length() ; i++) {
+			StringBuilder sb = new StringBuilder();
+			
+			for (int j = i ; j < s.length() ; j++) {
+				char ch = s.charAt(j);
+				sb.append(ch);
+				if(duplicate(sb.toString())){
+				    break;
+				}else{
+				    max = Math.max(max, sb.length());
+				}
+			}
+			
+			
+		}
+		return max ;
+		
+	}
+
+	//check Duplicates in O(26) 
+	boolean duplicate(String sh){
+	    boolean [] seen = new boolean[26];
+	    
+	    for(char ch : sh.toCharArray()){
+	        int idx = ch - 'a';
+	        if(seen[idx]){
+	            return true;
+	        }else{
+	            seen[idx]= true ;
+	        }
+	        
+	    }
+	    
+	    return false ;
+	}
+}
