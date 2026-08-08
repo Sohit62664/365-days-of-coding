e6c242bb4314829032f2428a387cd5ac79c4bb74
Sohit Sahu <sohit662006@gmail.com>
2026-08-09 00:01:19 +0530

    Update moveZeroes.java

--- Day220-08-08-2026/moveZeroes.java
+++ Day220-08-08-2026/moveZeroes.java
@@ -14,3 +14,22 @@ class Solution {
         }
     }
 }
+
+
+
+// Using Slow and Fast Pointer approach 
+class Solution {
+    public void moveZeroes(int[] nums) {
+        // using Slow and Fast pointer Approach
+
+        int slow = 0 ; 
+        for(int fast = 0 ; fast< nums.length ; fast++){
+            if(nums[fast]!=0){
+                int temp = nums[fast];
+                nums[fast]= nums[slow];
+                nums[slow]= temp ;
+                slow++;
+            }
+        }
+    }
+}
