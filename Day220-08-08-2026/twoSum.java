8de9b234a6c08ec058672ee9c91f5e54a2632e6b
Sohit Sahu <sohit662006@gmail.com>
2026-08-08 23:17:36 +0530

    Update and rename twoSum.java to Day220-08-08-2026/twoSum.java

--- Day220-08-08-2026/twoSum.java
+++ Day220-08-08-2026/twoSum.java
@@ -22,3 +22,21 @@ class Solution {
         return new int[]{-1,-1};
     }
 }
+
+
+// implemnted correct Hashmap Approacch 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i =0 ; i< nums.length ; i++){
+            int comp = target-nums[i];
+            if(map.containsKey(comp)){
+                return new int []{i , map.get(comp)};
+            }else{
+                map.put(nums[i] , i);
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
