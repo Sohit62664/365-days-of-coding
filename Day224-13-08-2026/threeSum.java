da65a42a37491629f607bfe61b8b13781bc3de0d
Sohit Sahu <sohit662006@gmail.com>
2026-08-13 00:29:15 +0530

    Adding with some important comments

--- Day224-13-08-2026/threeSum.java
+++ Day224-13-08-2026/threeSum.java
@@ -51,3 +51,65 @@ class Solution {
         return ans ;
     }
 }
+
+
+
+
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        // Goal is to Findout all the tripilits having sum = 0 ;
+
+        int n = nums.length ;
+        Arrays.sort(nums);
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        // Running till n-2 times we need two more elements: left and right.
+        for (int i  = 0 ; i< n-2 ; i++){
+
+            // skiping the Duplicate in the i 
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+
+
+            int num = nums[i];
+            int left = i+1 ; 
+            int right = n-1;
+            while(left<right){
+
+
+                int sum = nums[i] ;
+                sum+= nums[left];
+                sum += nums[right];
+
+                if(sum == 0 ){
+                    ans.add(Arrays.asList(nums[i] , nums[left] , nums[right]));
+                    //skipping duplicate for j 
+                    while(right > left && nums[right] == nums[right-1]){
+                        right--;
+                    }
+                    // Skippig duplicate for k 
+                    while(right> left && nums[left]== nums[left+1]){
+                        left++;
+                    }
+
+
+                    // always update them either duplicate available or not 
+                    left++;
+                    right--;
+                    
+                }else if(sum <0){
+                    left++;
+                }else if(sum> 0){
+                    right--;
+                }
+
+                
+            }
+        }
+
+        return ans ;
+    }
+}
