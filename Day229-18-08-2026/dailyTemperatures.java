ac63007ca498ae062337fb72ea2b2e47ff057f16
Sohit Sahu <sohit662006@gmail.com>
2026-08-18 23:20:32 +0530

    Monotonic Stack baced problem

---
+++ Day229-18-08-2026/dailyTemperatures.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n =temperatures.length ;
+        int ans [] = new int[n];
+        Stack<int []> st = new Stack<>();
+
+
+        for(int i = n -1 ; i>= 0 ; i--){
+            while(!st.isEmpty() && st.peek()[0] <= temperatures[i]){
+                st.pop();
+            }
+
+            if(st.isEmpty()){
+                ans[i]= 0 ;
+            }else{
+                ans[i] = st.peek()[1]- i ;
+            }
+
+            st.push(new int[]{temperatures[i] , i});
+        }
+
+        return ans;
+        
+    }
+}
