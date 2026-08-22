c723f98bc214d0fac337ba6656e209a66ff9161a
Sohit Sahu <sohit662006@gmail.com>
2026-08-22 10:41:58 +0530

    Adding another way to solve using recusion

--- Day233-22-08-2026/combintion.java
+++ Day233-22-08-2026/combintion.java
@@ -7,6 +7,7 @@ class Solution {
        return ans;
     }
 
+    // O(2^n)
     void recurse(int n , int i , int k , List<Integer> list){
         if(list.size()== k){
             ans.add(new ArrayList<>(list));
@@ -21,4 +22,34 @@ class Solution {
         list.remove(list.size()-1);
         recurse(n , i+1 , k , list);
     }
+}
+
+
+
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> combine(int n, int k) {
+       List<Integer> list = new ArrayList<>();
+       recurse(n , 0 ,k  , list);
+
+       return ans;
+    }
+
+    // O(k × C(n,k))
+    void recurse (int n , int start , int k , List<Integer> list){
+        if(list.size() == k ){
+            ans.add(new ArrayList <>(list));
+            return ;
+
+        }
+
+
+        for(int i= start  ; i< n ;i++){
+            list.add(i+1);
+            recurse(n , i+1 , k , list);
+
+            list.remove(list.size()-1);
+        }
+    }
+
 }
