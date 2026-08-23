716343eec1cb7b95016a446b1d7f67ecc7a2bcb5
Sohit Sahu <sohit662006@gmail.com>
2026-08-24 01:44:59 +0530

    Update firstMissingPositive.java

--- Day234-23-08-2026/firstMissingPositive.java
+++ Day234-23-08-2026/firstMissingPositive.java
@@ -54,3 +54,56 @@ class Solution {
         return -1 ;
     }
 }
+
+
+
+
+
+
+
+
+
+// Optimize Approach
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int st = 1 ;
+        int end = nums.length;
+
+
+        if(end==1){
+            if(nums[0]!= 1){
+                return 1 ;
+            }else{
+                return 2;
+            }
+        }
+        //clean the array
+        for(int i = 0 ;  i< nums.length ; i++){
+            if(nums[i] > end){
+                nums[i]= end+4;
+            }
+
+            if(nums[i]<=0){
+                nums[i]= end+4;
+            }
+        }
+
+        //Marking Negative
+        for(int i =0 ;i< nums.length ; i++){
+            int val = Math.abs(nums[i]);
+            if(val>=1 && val<= end)
+            nums[val-1]= -Math.abs(nums[val-1]);
+        }
+
+        for(int i =0 ; i < nums.length ; i++){
+            if(nums[i]>0){
+                return i+1;
+            }
+        }
+        return end+1;
+
+
+    }
+}
+
+
