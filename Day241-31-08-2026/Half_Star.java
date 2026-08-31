25b9e2562e3bea014e198e6cac546d1ef8d0365c
Sohit Sahu <sohit662006@gmail.com>
2026-08-31 22:51:47 +0530

    Create Half_Star.java

---
+++ Day241-31-08-2026/Half_Star.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        for(int i =0 ; i< n ; i++){
+            for(int j = 1 ; j < n-i ; j++){
+                System.out.print(" ");
+            }
+            
+            for(int j =0 ; j<= i ; j++){
+                System.out.print("*");
+            }
+            for(int j =0 ; j< i ; j++){
+                System.out.print("*");
+            }
+            
+            
+            
+            System.out.println();
+            
+        }
+
+        sc.close() ;
+    }
+}
