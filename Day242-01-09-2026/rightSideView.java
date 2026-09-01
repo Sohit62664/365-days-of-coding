438cfd149e4378ad14dd4ff9c7da44b208edcf37
Sohit Sahu <sohit662006@gmail.com>
2026-09-01 22:54:16 +0530

    Implement right side view of binary tree

---
+++ Day242-01-09-2026/rightSideView.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        //using Queue 
+
+        Deque<TreeNode> q = new ArrayDeque<>();
+        List<Integer> ans = new ArrayList<>();
+        if(root == null){
+            return ans;
+        }
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size() ;
+            for(int i = 0 ; i< size ; i++){
+                TreeNode curr = q.poll();
+                if(curr.left!= null){
+                    q.offer(curr.left);
+                }
+
+                if(curr.right!= null){
+                    q.offer(curr.right);
+                }
+                if(i == size-1){
+                    ans.add(curr.val);
+                }
+            }
+        }
+
+        return ans ;
+    }
+}
