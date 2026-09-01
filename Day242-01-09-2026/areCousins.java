8a6778a58a6b45aa7143eee4ec51e4ac2a4a7c63
Sohit Sahu <sohit662006@gmail.com>
2026-09-02 00:13:27 +0530

    Update areCousins.java

--- Day242-01-09-2026/areCousins.java
+++ Day242-01-09-2026/areCousins.java
@@ -55,3 +55,74 @@ class Solution {
 
     }
 }
+
+
+//Approach 02 
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCousins(TreeNode root, int x, int y) {
+
+        TreeNode xx = getNode(root, x);
+        TreeNode yy = getNode(root, y);
+        return (level(root, xx, 0) == level(root, yy, 0) && !isSibling(root, xx, yy));
+    }
+
+    TreeNode getNode(TreeNode root, int x) {
+        if (root == null)
+            return root;
+
+        if (root.val == x)
+            return root;
+
+        TreeNode n = getNode(root.left, x);
+        if (n != null)
+            return n;
+
+        return getNode(root.right, x);
+    }
+
+    boolean isSibling(TreeNode root, TreeNode x, TreeNode y) {
+        if (root == null)
+            return false;
+
+        if (root.left == x && root.right == y) {
+            return true;
+        } else if (root.left == y && root.right == x) {
+            return true;
+        }
+
+        boolean result = isSibling(root.left, x, y);
+        if (result)
+            return result;
+        return isSibling(root.right, x, y);
+    }
+
+    int level(TreeNode root, TreeNode xx, int l) {
+        if (root == null)
+            return 0;
+
+        if (root == xx)
+            return l;
+
+        int n = level(root.left, xx, l + 1);
+        if (n != 0)
+            return n;
+        return level(root.right, xx, l + 1);
+    }
+
+}
