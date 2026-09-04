84cabb027729811e8cd6c5bf9b066d5b891b7cce
Sohit Sahu <sohit662006@gmail.com>
2026-09-04 15:59:12 +0530

    Create kthSmallest.java

---
+++ Day245-04-09-2026/kthSmallest.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+    public int kthSmallest(TreeNode root, int k) {
+        helper(root);
+
+        for(int i = 0 ; i < k-1 ; i++){
+            pq.poll();
+        }
+
+        return pq.peek();
+
+    }
+
+    void helper(TreeNode root ){
+        if(root == null) return ;
+
+        pq.offer(root.val);
+
+        helper(root.left);
+        helper(root.right);
+    }
+}
