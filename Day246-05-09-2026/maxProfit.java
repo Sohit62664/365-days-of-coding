8fb9ee652c8f9d2a54cacb5941229276fb915987
Sohit Sahu <sohit662006@gmail.com>
2026-09-06 00:37:34 +0530

    Update maxProfit.java

--- Day246-05-09-2026/maxProfit.java
+++ Day246-05-09-2026/maxProfit.java
@@ -11,3 +11,23 @@ class Solution {
         return  max_profit;
     }
 }
+
+
+
+//Optimize approach
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max_profit =0 ;
+        
+        int min_price = prices[0];
+        for(int i = 1 ; i< prices.length ; i++){
+            int profit = prices[i]- min_price ;
+
+            min_price = Math.min(min_price , prices[i]);
+            max_profit = Math.max(max_profit , profit);
+        }
+
+        return max_profit;
+    }
+}
