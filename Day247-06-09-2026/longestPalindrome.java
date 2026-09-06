6edae941fa587fde6ba55b841bb0e2944de873a0
Sohit Sahu <sohit662006@gmail.com>
2026-09-06 17:36:03 +0530

    Update longestPalindrome.java

--- Day247-06-09-2026/longestPalindrome.java
+++ Day247-06-09-2026/longestPalindrome.java
@@ -38,3 +38,49 @@ class Solution {
         return true;
     }
 }
+
+
+
+
+//Correct Bruteforce Approach 
+
+class Solution {
+    public String longestPalindrome(String s) {
+        //Brute force 
+        // from i to j check for palindrom return the substring with max 
+        if (s.length() == 0)
+            return "";
+        int max_len = 1;
+        String ans = s.substring(0, 1);
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j < s.length(); j++) {
+                String sub = s.substring(i, j + 1);
+                if (isPalindrom(sub)) {
+                    if (max_len < sub.length()) {
+                        max_len = sub.length();
+                        ans = sub;
+                    }
+                }
+            }
+        }
+
+        return ans;
+
+    }
+
+    boolean isPalindrom(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i <= j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
