3ae8c4f9f9a2ba5c00d4617d2dae5fa9bddd593f
Sohit Sahu <sohit662006@gmail.com>
2026-09-06 11:40:12 +0530

    Update pairAndSum.java

--- Day247-06-09-2026/pairAndSum.java
+++ Day247-06-09-2026/pairAndSum.java
@@ -14,3 +14,38 @@ class Solution {
         return sum ;
     }
 }
+
+
+
+
+
+class Solution {
+    public long pairAndSum(int[] arr) {
+        // code here
+        
+        
+        long ans = 0 ;
+        for(int bit =0 ; bit< 31 ; bit++){
+            
+            // check sepratly for each  is it set or not in each number 
+            int count = 0 ; 
+            
+            for(int num : arr){
+                if((num & (1 << bit))  != 0 ){
+                    count++;
+                }
+            }
+            
+            // now calculate how many pair having the set bit in the total of count set bit number 
+            // Which is nothing bot the combinaton   count C 2 
+            
+            long  pairs = (long) count*(count-1 )/2;
+            
+            // now what is the total contribution of each pairs 
+            
+            ans+= pairs* (1L<<bit); // using 1L because of log
+        }
+        
+        return ans;
+    }
+}
