ff6d1b2ccd30a13c580a6283b2c5d1322e8b72c4
Sohit Sahu <sohit662006@gmail.com>
2026-09-06 15:54:53 +0530

    Update longestCommonPrefix.java

--- Day247-06-09-2026/longestCommonPrefix.java
+++ Day247-06-09-2026/longestCommonPrefix.java
@@ -28,3 +28,26 @@ class Solution {
         return sb.toString();
     }
 }
+
+
+
+//Optimal Approach
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+        int idx = 0;
+
+        System.out.println(Arrays.toString(strs));
+        while(idx < s1.length() && idx < s2.length()){
+            if(s1.charAt(idx) == s2.charAt(idx)){
+                idx++;
+            } else {
+                break;
+            }
+        }
+        return s1.substring(0, idx);
+    }
+}
