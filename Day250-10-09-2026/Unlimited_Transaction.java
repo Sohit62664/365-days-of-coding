3e2f93254be37498d8440e81e4280bd8fa18bea4
Sohit Sahu <sohit662006@gmail.com>
2026-09-10 16:49:13 +0530

    Adding Optimize Approiach

--- Day250-10-09-2026/Unlimited_Transaction.java
+++ Day250-10-09-2026/Unlimited_Transaction.java
@@ -39,3 +39,30 @@ class Solution {
         }
     }
 }
+
+
+
+
+
+// optimize Approach 
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        // code here
+        // optimize Approach 
+        
+        // sell if curr price > last pricr
+        
+        int profit =0 ; 
+        
+        for(int i =1 ; i< prices.length ; i++ ){
+            if(prices[i]> prices[i-1]){
+                profit+= prices[i] - prices[i-1];
+            }
+        }
+        
+        return profit ;
+    }
+}
+
