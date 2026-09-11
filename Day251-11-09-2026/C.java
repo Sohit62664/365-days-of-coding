8068e22f3ec108516e78388faeb928d6388dc017
Sohit Sahu <sohit662006@gmail.com>
2026-09-11 23:44:26 +0530

    Update C.java

--- Day251-11-09-2026/C.java
+++ Day251-11-09-2026/C.java
@@ -1,3 +1,55 @@
+// Correct Accepted Version
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class C {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+
+
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            for(int i =0 ; i< n ; i++){
+                if(a[i]== -1 ) a[i]= 1 ;
+                if(a[i] == 1 ) break ;
+
+            }
+
+            for(int i = n-1 ; i>= 0 ; i--){
+                if(a[i]==-1) a[i]=1 ;
+                if(a[i]== 1 ) break;
+            }
+
+
+            for(int i =0 ; i< n ; i++){
+                if(a[i]==-1){
+                    a[i]= 0;
+                }
+            }
+
+            for(int i : a){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+
+            // System.out.println(Arrays.toString(a));
+        }
+    }
+}
+
+
+
+//-------------------------------------------------******************************---------------------------------------------------
+
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
