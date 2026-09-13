50dba71bd4f230f44135c6c5debd99e15e7089e8
Sohit Sahu <sohit662006@gmail.com>
2026-09-13 12:31:36 +0530

    Update findMajority_2.java

--- Day253-13-09-2026/findMajority_2.java
+++ Day253-13-09-2026/findMajority_2.java
@@ -62,3 +62,70 @@ class Solution {
 	}
 }
 
+
+
+
+//More Optimized Boyer-Moors algo 
+
+
+class Solution {
+	public ArrayList<Integer> findMajority(int[] arr) {
+		// code here
+		int n = arr.length;
+		ArrayList<Integer> result = new ArrayList<>();
+		
+		int el1 = -1;
+		int el2 = -1;
+		
+		int cnt1 = 0 ;
+		int cnt2 = 0 ;
+		
+		for (int num : arr) {
+			
+			if (num == el1) {
+				cnt1++;
+			} else if (num == el2) {
+				cnt2++;
+			} else
+			
+			if (cnt1 == 0) {
+				el1 = num ;
+				cnt1++;
+				
+			} else if (cnt2 == 0) {
+				cnt2++;
+				el2 = num ;
+			} else {
+				cnt1--;
+				cnt2--;
+			}
+		}
+		
+		int f1 = 0 ;
+		int f2 = 0 ;
+		
+		for (int num : arr) {
+			if (num == el1) {
+				f1++;
+			}
+			if (num == el2) {
+				f2++;
+			}
+			
+		}
+		
+		if (f1 > arr.length/3) {
+			result.add(el1);
+		}
+		if (f2 > arr.length/3) {
+			result.add(el2);
+		}
+	    
+	    Collections.sort(result);
+	    
+		return result;
+		
+		
+	
+	}
+}
