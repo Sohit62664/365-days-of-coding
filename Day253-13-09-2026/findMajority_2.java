bee85f69d3d948b363c3f4160ec59bbf5da489c4
Sohit Sahu <sohit662006@gmail.com>
2026-09-13 11:40:28 +0530

    naive_Approach

---
+++ Day253-13-09-2026/findMajority_2.java
@@ -0,0 +1,33 @@
+class Solution {
+    public ArrayList<Integer> findMajority(int[] arr) {
+        // code here
+        // naive approach O(n^2);
+        
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>(); // Handeling Duplicates
+        
+        int n = arr.length ; 
+        
+        for(int i=0 ; i< n ; i++){
+            int f = 0 ;
+            int num = arr[i];
+            if(set.contains(num)){
+                continue;
+            }
+            for(int j = 0 ; j< n ; j++){
+                if(num== arr[j]){
+                    f++;
+                }
+            }
+            set.add(num);
+            if(f>(n/3)){
+                result.add(num);
+            }
+        }
+        
+        Collections.sort(result);
+        
+        return result;
+    }
+}
