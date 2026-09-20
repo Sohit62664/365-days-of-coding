5c18f06b7b8dd867db53aeadd655941cb56e5cd7
Sohit Sahu <sohit662006@gmail.com>
2026-09-20 21:37:05 +0530

    Create reverseDegree.java

---
+++ Day259-09-2026/reverseDegree.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverseDegree(String s) {
+        // 'a'-> = 123-97 = 26 
+        // 123- ascii value = 
+
+        int sum = 0;
+        for(int i= 0; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            int temp = (123 - ch);
+            sum+= temp*(i+1);
+        }
+
+        return sum;
+    }
+}
