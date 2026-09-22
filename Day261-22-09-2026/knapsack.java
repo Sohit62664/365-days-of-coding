0fb794d1fac10529f2c21f04e0c23c2ac561508f
Sohit Sahu <sohit662006@gmail.com>
2026-09-22 08:36:33 +0530

    Create knapsack.java

---
+++ Day261-22-09-2026/knapsack.java
@@ -0,0 +1,16 @@
+class Solution {
+	public int knapsack(int W, int val[], int wt[]) {
+		// code here
+		int n = wt.length;
+		
+		int [] dp = new int[W + 1];
+		
+		for (int i = 0 ; i<n ; i++) {
+			for (int w = W ; w >= wt[i] ; w--) {
+				dp[w] = Math.max(dp[w], val[i] + dp[w - wt[i]]);
+			}
+		}
+		
+		return dp[W];
+	}
+}
